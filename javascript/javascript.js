121747b4584c80356ee3dd9da85e5a3c2ca4a2ab
fjordski <fordheacock@gmail.com>
2017-03-22 08:11:09 -0500

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,23 +1,21 @@
-	$(document).ready(function() {
+$(document).ready(function() {
+$('.gallery-item').magnificPopup({
+type: 'image',
+gallery:{
+enabled:true,
+tCounter: '<span class="mfp-counter"></span>'
+}
+});
 
-			
-		$('.gallery-item').magnificPopup({
-	  type: 'image',
-	  gallery:{
-		enabled:true,
-		tCounter: '<span class="mfp-counter"></span>'  
-	  }
-	});
-		
-		$("#search").keyup(function(){
-      var current_query = $(this).val();
-      $('.gallery-item').hide();
-      $('.gallery-item').each(function(){
-        var current_keyword = $(this).attr("data-sub-html");
-        if(current_keyword.indexOf(current_query) >= 0){
-          $(this).show();
-        }
-      }); 
-    });
-				
+$("#search").keyup(function(){
+var current_query = $(this).val();
+$('.gallery-item').hide();
+$('.gallery-item').each(function(){
+var current_keyword = $(this).attr("data-sub-html");
+if(current_keyword.indexOf(current_query) >= 0){
+$(this).show();
+}
+});
+});
+
 });
